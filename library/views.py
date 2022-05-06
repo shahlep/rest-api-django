23cf19e6f1762b31c3f0301da98064fa87022785
Shah rahman <toshah.rahman@gmail.com>
2022-05-06 18:56:49 +0300

    setting up rest framework step 8

--- library/views.py
+++ library/views.py
@@ -6,9 +6,13 @@ from rest_framework.decorators import api_view
 
 @api_view('GET', 'POST')
 def book_list(request):
-    # get all the books
-    books = Library.objects.all()
-    # serialize them
-    serializer = LibrarySerializer(books, many=True)
-    # return json
-    return JsonResponse(serializer.data, safe=False)
+    if request.method == 'GET':
+        # get all the books
+        books = Library.objects.all()
+        # serialize them
+        serializer = LibrarySerializer(books, many=True)
+        # return json
+        return JsonResponse(serializer.data, safe=False)
+
+    if request.method == 'POST':
+        serializer = LibrarySerializer(data=request.data)

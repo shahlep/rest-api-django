6901ff9a1e8cf017bca4e00e2852e3a548ac2f95
Shah rahman <toshah.rahman@gmail.com>
2022-05-06 18:15:10 +0300

    add admin file and add method in model

--- library/models.py
+++ library/models.py
@@ -5,3 +5,6 @@ class Library(models.Model):
     book_name = models.CharField(max_length=100)
     isbn = models.CharField(max_length=5)
     aisle = models.IntegerField()
+
+    def __str__(self):
+        return self.book_name

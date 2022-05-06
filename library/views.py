20ed9649589b863185cceed75f56f32fb31d46f6
Shah rahman <toshah.rahman@gmail.com>
2022-05-06 18:30:09 +0300

    setting up rest framework step 4

--- library/views.py
+++ library/views.py
@@ -2,7 +2,11 @@ from django.http import JsonResponse
 from .models import Library
 from .serializers import LibrarySerializer
 
+
 def book_list(request):
-    #get all the books
-    #serialize them
-    #return json
+    # get all the books
+    books = Library.objects.all()
+    # serialize them
+    serializer = LibrarySerializer(books, many=True)
+    # return json
+    return JsonResponse(serializer.data)

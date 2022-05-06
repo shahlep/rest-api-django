c9969214fb60412257e0f2ec5ce8add1e7b521be
Shah rahman <toshah.rahman@gmail.com>
2022-05-06 18:06:30 +0300

    run makemigrations for app

--- library/models.py
+++ library/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Library(models.Model):
     book_name = models.CharField(max_length=100)
     isbn = models.CharField(max_length=5)
-    aisle = models.IntegerField(max_length=4)
+    aisle = models.IntegerField()

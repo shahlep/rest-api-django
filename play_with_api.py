27c4551cb1a8b5e412514ee4a6b982dc7d0c5f3f
Shah rahman <toshah.rahman@gmail.com>
2022-05-08 09:37:10 +0300

    request fun 1

--- play_with_api.py
+++ play_with_api.py
@@ -1,5 +1,12 @@
 import requests
 
-response = requests.get('http://127.0.0.1:8000/library')
+response = requests.get('http://127.0.0.1:8000/library/')
 
-print(response.json())
+dict_response = response.json()
+print(dict_response[0])
+print(dict_response[0]['book_name'])
+print(response.status_code)
+#print(response.headers)
+print(response.headers['Content-Type'])
+
+
